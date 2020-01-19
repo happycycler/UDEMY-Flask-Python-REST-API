cc03899ca930d1331bb4d7a1b1fd83d7b32176ae
Mark Price <mrprice1@comcast.net>
2020-01-19 16:47:34 -0500

    Fixed error due to not importing db when running the app.

--- app.py
+++ app.py
@@ -12,15 +12,11 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://store_api_dev:password@localhost/store_api?sslmode=disable'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/api_db'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mrprice:HighwayToH3!!@107.180.51.82/oslapi'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mrprice:HighwayToH3!!@107.180.51.82/oslapi '
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secretkey'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
