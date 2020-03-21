f8eeadac698b188d7c9459c47638ea502316a63f
Mark Price <mrprice1@comcast.net>
2020-03-21 00:58:23 -0400

    Updated course POST functionality.

--- resources/course.py
+++ resources/course.py
@@ -103,8 +103,8 @@ class Course(Resource):
         enddate = datetime.strptime(data['enddate'], "%Y-%m-%d") + timedelta(days=1)
         # starttime = datetime.strptime(data['starttime'], "%I:%M %p") if system() == 'Windows' else datetime.strptime(data['starttime'], "%-I:%M %p")
         # endtime = datetime.strptime(data['endtime'], "%I:%M %p") if system() == 'Windows' else datetime.strptime(data['endtime'], "%-I:%M %p")
-        starttime = datetime.strptime(data['starttime'], "%#I:%M %p")
-        endtime = datetime.strptime(data['endtime'], "%#I:%M %p")
+        starttime = datetime.strptime(data['starttime'], "%I:%M %p")
+        endtime = datetime.strptime(data['endtime'], "%I:%M %p")
         orgid = data['orgid']
         userid = data['userid']
         if data['classdays'] == None:
